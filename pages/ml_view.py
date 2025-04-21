2479fc292c0f63c83edf95b31abc579ee45e87e1
vishnupv7 <103145321+vishnupv7@users.noreply.github.com>
2025-04-21 10:18:54 +0530

    Update ml_view.py

--- pages/ml_view.py
+++ pages/ml_view.py
@@ -5,7 +5,8 @@ import pandas as pd
 import plotly.express as px
 import os
 
-def app():
+def app(df):
+
     st.title("🤖 ML Anomaly Detection")
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(ROOT_DIR, "../data/processed/etihad_ml_anomaly_flags.csv")
