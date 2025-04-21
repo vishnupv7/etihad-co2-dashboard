741072950b51ce3131f613f0925806f41ddc5be2
vishnupv7 <103145321+vishnupv7@users.noreply.github.com>
2025-04-21 10:19:28 +0530

    Update weather_view.py

--- pages/weather_view.py
+++ pages/weather_view.py
@@ -5,7 +5,8 @@ import pandas as pd
 import plotly.express as px
 import os
 
-def app():
+def app(df):
+
     st.title("🌦️ Weather Impact Insights")
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(ROOT_DIR, "../data/processed/etihad_weather_adjusted.csv")
