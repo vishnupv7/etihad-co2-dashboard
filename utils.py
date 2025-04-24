f47a7bcca6cb342943046b8495fa669b50ef6e04
vishnupv7 <103145321+vishnupv7@users.noreply.github.com>
2025-04-24 18:11:12 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-
-def load_dashboard_data():
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    csv_path = os.path.abspath(os.path.join(base_dir, "../data/processed/final_dashboard_dataset.csv"))
-    return pd.read_csv(csv_path, low_memory=False)
